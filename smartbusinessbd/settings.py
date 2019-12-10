ac0fb6b3d3287a24d151710cae5a750a9330b55f
Fahim <recentfahim13@gmail.com>
2019-12-10 18:31:32 +0600

    remove linkedin and twitter from authentication

--- smartbusinessbd/settings.py
+++ smartbusinessbd/settings.py
@@ -39,9 +39,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.linkedin',
-    'allauth.socialaccount.providers.linkedin_oauth2',
-    'allauth.socialaccount.providers.twitter',
     'corsheaders',
 ]
 
